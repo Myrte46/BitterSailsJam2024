ea8992c07d6f1efef5dd4009d9f0fb59c1181dd1
Myrte46 <mfborst12@gmail.com>
2024-01-18 10:22:29 +0100

    I know a guy

--- Assets/Scripts/ToGame.cs
+++ Assets/Scripts/ToGame.cs
@@ -7,7 +7,7 @@ public class ToGame : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        if (Input.GetKeyDown(KeyCode.E))
         {
             UnityEngine.SceneManagement.SceneManager.LoadScene("SpeakingScene");
         }
